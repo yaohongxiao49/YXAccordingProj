d9dbcd78734ee70d28e199e4adfacdbc8f876476
yaohongxiao <617146817@qq.com>
2021-06-02 15:05:10 +0800

    change：up

---
+++ YXAccordingProj/HomeModule/View/YXHomeAlertView/YXHomeAlertView.swift
@@ -0,0 +1,24 @@
+//
+//  YXHomeAlertView.swift
+//  YXAccordingProj
+//
+//  Created by ios on 2021/6/2.
+//
+
+import SwiftUI
+
+struct YXHomeAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.body)
+            .foregroundColor(Color.black)
+            .multilineTextAlignment(.leading)
+            .frame(minWidth: 0, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 0, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YXHomeAlertView_Previews: PreviewProvider {
+    static var previews: some View {
+        YXHomeAlertView()
+    }
+}
