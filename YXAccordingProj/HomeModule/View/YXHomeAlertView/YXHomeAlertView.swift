8c84e31c42be046f45903f246338cbfb49e9d018
yaohongxiao <617146817@qq.com>
2021-07-16 15:03:13 +0800

    change：up

--- YXAccordingProj/HomeModule/View/YXHomeAlertView/YXHomeAlertView.swift
+++ YXAccordingProj/HomeModule/View/YXHomeAlertView/YXHomeAlertView.swift
@@ -2,23 +2,58 @@
 //  YXHomeAlertView.swift
 //  YXAccordingProj
 //
-//  Created by ios on 2021/6/2.
+//  Created by ios on 2021/6/30.
 //
 
-import SwiftUI
+import UIKit
 
-struct YXHomeAlertView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .font(.body)
-            .foregroundColor(Color.black)
-            .multilineTextAlignment(.leading)
-            .frame(minWidth: 0, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 0, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-    }
-}
+class YXHomeAlertView: UIView, UITableViewDelegate, UITableViewDataSource {
 
-struct YXHomeAlertView_Previews: PreviewProvider {
-    static var previews: some View {
-        YXHomeAlertView()
+    lazy var tableView: UITableView = {
+       
+        let tableView = UITableView.init(frame: self.bounds, style: UITableView.Style.plain)
+        tableView.estimatedRowHeight = 30
+        tableView.backgroundColor = UIColor.white
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        self.addSubview(tableView)
+        
+        tableView.register(YXHomeAlertViewCell.classForCoder(), forCellReuseIdentifier: NSStringFromClass(YXHomeAlertViewCell.classForCoder()))
+        
+        return tableView
+    }()
+    
+    //MARK:- UITableViewDelegate, UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: YXHomeAlertViewCell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(YXHomeAlertViewCell.classForCoder()), for: indexPath) as! YXHomeAlertViewCell
+        
+        return cell
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK:- 初始化视图
+    func initView() {
+                
+        self.tableView.reloadData()
+    }
+    
 }
---
+++ YXAccordingProj/HomeModule/View/YXHomeAlertView/YXHomeAlertViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  YXHomeAlertViewCell.swift
+//  YXAccordingProj
+//
+//  Created by ios on 2021/6/30.
+//
+
+import UIKit
+
+class YXHomeAlertViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
